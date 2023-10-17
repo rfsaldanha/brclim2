967a0dc13045a87cbb13045299d9a7f6cc665283
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-17 08:45:22 +0200

    targets projects

---
+++ projects_conf.R
@@ -0,0 +1,5 @@
+### Projects configuration
+# targets::tar_config_set(script = "brazil_1950_2022.R", store = "brazil_1950_2022", project = "brazil_1950_2022")
+
+### Project selector
+Sys.setenv(TAR_PROJECT = "brazil_1950_2022")
