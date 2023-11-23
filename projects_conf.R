19a0b702bfb9bf98f0fbd74d517233549d250220
Raphael Saldanha <rfsaldanha@gmail.com>
2023-11-23 09:51:05 +0100

    mozambique

--- projects_conf.R
+++ projects_conf.R
@@ -2,9 +2,11 @@
 # targets::tar_config_set(script = "brazil_1950_2022.R", store = "brazil_1950_2022", project = "brazil_1950_2022")
 # targets::tar_config_set(script = "brazil_2000_2020_wpop.R", store = "brazil_2000_2020_wpop", project = "brazil_2000_2020_wpop")
 # targets::tar_config_set(script = "brazil_2023.R", store = "brazil_2023", project = "brazil_2023")
+# targets::tar_config_set(script = "mozambique_2000_2022.R", store = "mozambique_2000_2022", project = "mozambique_2000_2022")
 
 ### Project selector
 # Sys.setenv(TAR_PROJECT = "brazil_2023")
+# Sys.setenv(TAR_PROJECT = "mozambique_2000_2022")
 # Sys.setenv(TAR_PROJECT = "brazil_1950_2022")
 # Sys.setenv(TAR_PROJECT = "brazil_2000_2020_wpop")
 
